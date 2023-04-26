b037411d89218224f25a75550222c89a3991b3bb
Egnal Zurc <acmlsn@gmail.com>
2023-04-26 11:13:26 +0200

    Hotel controller unit tests

---
+++ expedia-endpoints/src/test/java/com/expedia/hotel/HotelControllerTest.java
@@ -0,0 +1,40 @@
+package com.expedia.hotel;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class HotelControllerTest {
+
+    @Mock
+    private HotelReadDelegate hotelReadDelegate;
+
+    private HotelController hotelController;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        hotelController = new HotelController(hotelReadDelegate);
+    }
+
+    @Test
+    public void testGetHotelsOrderByRating() {
+        List<Hotel> hotels = new ArrayList<>();
+        when(hotelReadDelegate.readResources()).thenReturn(hotels);
+
+        List<Hotel> result = hotelController.query();
+
+        verify(hotelReadDelegate, times(1)).readResources();
+        assertEquals(hotels, result);
+    }
+}
